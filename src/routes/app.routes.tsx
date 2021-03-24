02773367d3aececeea4b413c5356198eba0c3945
Hugo Souza <hugo.souza@ifood.com.br>
2021-03-24 09:50:25 -0300

    Add new pages structure

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { createStackNavigator  } from '@react-navigation/stack';
 
 import Dashboard from '../Pages/Dashboard';
+import Profile from '../Pages/Profile';
+import CreateAppointment from '../Pages/CreateAppointment';
+import AppointmentCreated from '../Pages/AppointmentCreated';
 
 
 const App = createStackNavigator();
@@ -14,6 +17,10 @@ export const AppRoutes: React.FC = () => (
     }}
   >
     <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="CreateAppointment" component={CreateAppointment} />
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+
+    <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
 );
 
